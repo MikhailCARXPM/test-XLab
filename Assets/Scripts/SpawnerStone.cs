86bfc39a2245d05adcb88b8c810b9ee4a1890450
MikhailCARXPM <drozdov.misha89@gmail.com>
2024-12-28 16:01:57 +0300

    падение камней

---
+++ Assets/Scripts/SpawnerStone.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+namespace Golf
+{
+    public class SpawnerStone : MonoBehaviour
+    {
+        public GameObject[] prefabs;
+
+        public void Spawn()
+        {
+            var prefab = GetRandomPrefab();
+
+            if (prefab == null)
+            {
+                return;
+            }
+            Instantiate(prefab, transform.position, Quaternion.identity);
+        }
+
+        private GameObject GetRandomPrefab()
+        {
+            if (prefabs.Length == 0)
+            {
+                return null;
+            }
+
+            int index = Random.Range(0, prefabs.Length);
+            return prefabs[index];
+        }
+    }
+}
